4976f8bfef25b1179b714a7eba4a95a5c004ab45
Harings Rob <haringsrob@gmail.com>
2022-08-21 09:25:00 +0200

    New version info.

--- app/Logger.php
+++ app/Logger.php
@@ -9,10 +9,10 @@ class Logger
 {
     public static function getLogger(): LoggerInterface
     {
-        /* return new NullLogger(); */
+        return new NullLogger();
         // The below is usefull for development.
         // @todo: Use this via a switch?
-        return new \Wa72\SimpleLogger\FileLogger('/Users/rob/Sites/laravel-dev-generators/log.txt');
+        //return new \Wa72\SimpleLogger\FileLogger('/Users/rob/Sites/laravel-dev-generators/log.txt');
     }
 
     public static function logException(\Exception $e): void

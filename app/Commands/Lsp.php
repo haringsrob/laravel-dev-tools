3dd6587800005bf2e57fda307b9eab3ff1a914ed
Harings Rob <haringsrob@gmail.com>
2023-05-08 21:33:48 +0200

    Vardumper reformatting.

--- app/Commands/Lsp.php
+++ app/Commands/Lsp.php
@@ -21,16 +21,16 @@ class Lsp extends Command
 
     public function handle(): void
     {
-$cloner = new VarCloner();
-$fallbackDumper = \in_array(\PHP_SAPI, ['cli', 'phpdbg']) ? new CliDumper() : new HtmlDumper();
-$dumper = new ServerDumper('tcp://127.0.0.1:9912', $fallbackDumper, [
-    'cli' => new CliContextProvider(),
-    'source' => new SourceContextProvider(),
-]);
+        $cloner = new VarCloner();
+        $fallbackDumper = \in_array(\PHP_SAPI, ['cli', 'phpdbg']) ? new CliDumper() : new HtmlDumper();
+        $dumper = new ServerDumper('tcp://127.0.0.1:9912', $fallbackDumper, [
+            'cli' => new CliContextProvider(),
+            'source' => new SourceContextProvider(),
+        ]);
 
-VarDumper::setHandler(function ($var) use ($cloner, $dumper) {
-    $dumper->dump($cloner->cloneVar($var));
-});
+        VarDumper::setHandler(function ($var) use ($cloner, $dumper) {
+            $dumper->dump($cloner->cloneVar($var));
+        });
 
 
         $logger = Logger::getLogger();
